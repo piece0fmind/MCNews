8076497342fa4473dd6dd93e820f74103dae5cf5
piece0fmind <bibek9lives@icloud.com>
2025-01-06 22:29:35 +0545

    feat: Add entity AppUser

---
+++ API/Domain/AppUser.cs
@@ -0,0 +1,17 @@
+﻿
+namespace API.Domain
+{
+    public class AppUser 
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; } 
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Phone { get; set; }
+        public bool IsEmailConfirmed { get; set; }
+        public bool IsActive { get; set; }
+
+    }
+}
--- API/Infrastructure/AppDbContext.cs
+++ API/Infrastructure/AppDbContext.cs
@@ -1,16 +1,18 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using API.Domain;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace API
+namespace API.Infrastructure
 {
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions options) : base(options)
         {
         }
+        public DbSet<AppUser> Users { get; set; }
     }
 }
