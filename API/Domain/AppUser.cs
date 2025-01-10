e25699114b46cc87f00a0bde518213270abc785a
piece0fmind <bibek9lives@icloud.com>
2025-01-10 11:12:13 +0545

    Add UserName field in AppUser entity

--- API/Domain/AppUser.cs
+++ API/Domain/AppUser.cs
@@ -6,6 +6,7 @@ namespace API.Domain
         public Guid Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; } 
+        public string UserName { get; set; } 
         public DateTime CreatedDate { get; set; } = DateTime.Now;
         public string Email { get; set; }
         public string Password { get; set; }
