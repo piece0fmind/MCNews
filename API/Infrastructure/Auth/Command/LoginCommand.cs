a031b691cdc4053a47ddc8d2cfa1e4401c957705
bibekneupanewealthlane <bibek.neupane@wealthlane.co>
2025-01-10 06:51:37 +0545

    Create LoginCommandHandler

---
+++ API/Infrastructure/Auth/Command/LoginCommand.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Infrastructure.Auth.Command
+{
+    public class LoginCommand : IRequest<LoginResponse>
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+
+    public record LoginResponse(Guid UserId, string Name, string Token);
+
+    public class LoginCommandHandler : IRequestHandler<LoginCommand, LoginResponse>
+    {
+        public Task<LoginResponse> Handle(LoginCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
