b04ead69c66a3ff6bd4390de432da50669ba1c7e
piece0fmind <bibek9lives@icloud.com>
2025-01-10 12:24:16 +0545

    Mock login and throw dummy token

--- API/Infrastructure/Auth/Command/LoginCommand.cs
+++ API/Infrastructure/Auth/Command/LoginCommand.cs
@@ -1,6 +1,11 @@
-﻿using MediatR;
+﻿using API.Domain;
+using API.Shared;
+using FluentValidation;
+using MediatR;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,11 +18,59 @@ namespace API.Infrastructure.Auth.Command
         public string Password { get; set; }
     }
 
-    public record LoginResponse(Guid UserId, string Name, string Token);
+    public class LoginResponse : ApiResponse
+    {
+        public Guid UserId { get; set; }
+        public string UserName { get; set; } = string.Empty;
+        public string Token {  get; set; }
+    }
+        
 
     public class LoginCommandHandler : IRequestHandler<LoginCommand, LoginResponse>
     {
-        public Task<LoginResponse> Handle(LoginCommand request, CancellationToken cancellationToken)
+        private readonly AppDbContext _dbContext;
+        private readonly IMediator _mediator;
+
+        public LoginCommandHandler(AppDbContext dbContext, IMediator mediator)
+        {
+            _dbContext = dbContext;
+            _mediator = mediator;
+        }
+
+        public async Task<LoginResponse> Handle(LoginCommand request, CancellationToken cancellationToken)
+        {
+            // validate user
+            var user = _dbContext.Users.FirstOrDefault(x => x.UserName == request.UserName);
+            if (user is null)
+            {
+                return new LoginResponse
+                {
+                    Success = false,
+                    Message = "User not found"
+                };
+            }
+
+            var loginResponse = new LoginResponse
+            {
+                Success = true,
+                Message = "Successfully logged in",
+                UserId = user.Id,
+                UserName = user.UserName,
+                Token = GenerateToken(user)
+            };
+
+            return loginResponse;
+        }
+        private static string GenerateToken(AppUser user)
+        {
+            //generate token
+            var token = "token";
+            return token;
+        }
+    }
+    public class LoginCommandValidator : AbstractValidator<LoginCommand>
+    {
+        public LoginCommandValidator()
         {
             throw new NotImplementedException();
         }
