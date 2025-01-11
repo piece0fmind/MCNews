cf48fd70389e2e54a64d9375a42b31081f91e69e
piece0fmind <bibek9lives@icloud.com>
2025-01-12 00:33:06 +0545

    Create new Role and UserRole tables

---
+++ Application/Domain/UserRole.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Domain
+{
+    public class UserRole
+    {
+        public Guid UserId { get; set; }
+        public Guid RoleId { get; set; }
+        public AppUser User { get; set; }
+        public Role Role { get; set; }
+
+    }
+}
--- Application/Infrastructure/AppDbContext.cs
+++ Application/Infrastructure/AppDbContext.cs
@@ -16,5 +16,30 @@ namespace Application.Infrastructure
         }
         public DbSet<AppUser> Users { get; set; }
         public DbSet<RefreshToken> RefreshTokens { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<UserRole> UserRoles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<UserRole>()
+                .HasKey(key => new { key.UserId, key.RoleId});
+
+            modelBuilder.Entity<Role>()
+                .HasData(
+                new { Id = new Guid("2b6fb76c-6d7f-49dc-8e1d-44432f8b03b9"), Name = "User" },
+                new { Id = new Guid("a5ad34b9-a72a-4063-9738-de3321123c4c"), Name = "Admin" });
+
+            modelBuilder.Entity<UserRole>()
+                .HasOne(x => x.User)
+                .WithMany(x => x.UserRoles)
+                .HasForeignKey(x => x.UserId);
+
+            modelBuilder.Entity<UserRole>()
+                .HasOne(x => x.Role)
+                .WithMany(x => x.UserRoles)
+                .HasForeignKey(x => x.RoleId);
+        }
     }
 }
