c536c28752e1efc6dd0f2bfb772bcede6549e3d2
piece0fmind <bibek9lives@icloud.com>
2025-01-12 16:12:01 +0545

    Implement password hashing with BCrypt

---
+++ Application/Infrastructure/PasswordHasher/PasswordHasher.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Application.Infrastructure
+{
+    public class PasswordHasher : IPasswordHasher
+    {
+        private const int hashWorkFactor = 10;
+        
+        public string Hash(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password, hashWorkFactor);
+        }
+
+    }
+}
