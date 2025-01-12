d16ae9c60c655e07f7a7d34d834cb799bfd12b2f
piece0fmind <bibek9lives@icloud.com>
2025-01-12 16:12:22 +0545

    Implement password validator

---
+++ Application/Infrastructure/PasswordValidator/PasswordValidator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Infrastructure.PasswordValidator
+{
+    public class PasswordValidator : IPasswordValidator
+    {
+        public bool ValidatePassword(string password, string hashedPassword)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, hashedPassword);
+        }
+    }
+}
